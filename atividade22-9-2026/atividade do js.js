d74d9421a18649cb6c9f892d1ed2909221e12abd
joao-pedro1234567890 <joaopedro1marciano@gmail.com>
2026-09-22 16:23:45 -0300

    Add files via upload

--- atividade22-9-2026/atividade do js.js
+++ atividade22-9-2026/atividade do js.js
@@ -4,5 +4,12 @@ let altura=Number(prompt("dijite a altura"));
 let area=base*altura;
 alert(" a area e de "+area);
 }
+function atv2(){
+let ehPar=Number(prompt("dijite o numero"));
+ehPar=ehPar%2;
+console.log(ehPar);
+if(ehPar===0){alert("e par")}
+else{alert("e impar")}
+}
 
 
