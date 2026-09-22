8d9fea530c6334798d9cd27bc4bac26c1a1eacd4
joao-pedro1234567890 <joaopedro1marciano@gmail.com>
2026-09-22 17:03:00 -0300

    Add files via upload

--- atividade22-9-2026/atividade do js.js
+++ atividade22-9-2026/atividade do js.js
@@ -5,17 +5,19 @@ let area=base*altura;
 alert(" a area e de "+area);
 }
 function atv2(){
-let ehPar=Number(prompt("dijite o numero"));
-ehPar=ehPar%2;
-console.log(ehPar);
-if(ehPar===0){alert("e par")}
-else{alert("e impar")}
+    let ehPar=Number(prompt("dijite o numero"));
+    ehPar=ehPar%2;
+    console.log(ehPar);
+    if(ehPar===0){
+        alert("e par")}
+    else{
+        alert("e impar")}
 }
 function atv3(){
-let C=Number(prompt("dijite pra ser convertido pra farenhaite"));
-let F=(C*1.8)+32;
-console.log(F+" "+C);
-alert("deu "+F);
+    let C=Number(prompt("dijite pra ser convertido pra farenhaite"));
+    let F=(C*1.8)+32;
+    console.log(F+" "+C);
+    alert("deu "+F);
 }
 function atv4(){
 let peso=Number(prompt("dijete o peso"));
@@ -32,5 +34,11 @@ let vet=[Number(prompt("dijite o numero")),Number(prompt("dijite o numero")),Num
 let soma=vet[0]+vet[1]+vet[2]
 console.log(soma);
 alert("deu "+soma)
+}
+function atv6(){
+let pessoa={ nome:prompt("nome"), idade: prompt("idade"), clt: prompt("CLT")}
+alert("Olá, meu nome é "+pessoa.nome +" tenho "+pessoa.idade +" anos e trabalho como "+pessoa.clt )
+
+
 }
 
