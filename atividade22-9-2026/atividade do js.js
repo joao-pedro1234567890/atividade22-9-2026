bfabb5f270137e3cf1f22f78e01bebd432fe0efa
joao-pedro1234567890 <joaopedro1marciano@gmail.com>
2026-09-22 16:48:01 -0300

    Add files via upload

--- atividade22-9-2026/atividade do js.js
+++ atividade22-9-2026/atividade do js.js
@@ -26,4 +26,11 @@ if(imc<18.5){alert("abaixo do peso");};
 if(imc>25){alert("acima do peso");};
 if(imc<25 &&imc>18.5){alert("peso ideal");};
 }
+function atv5(){
+    alert("e um vetor de 3 numeros")
+let vet=[Number(prompt("dijite o numero")),Number(prompt("dijite o numero")),Number(prompt("dijite o numero")),]
+let soma=vet[0]+vet[1]+vet[2]
+console.log(soma);
+alert("deu "+soma)
+}
 
