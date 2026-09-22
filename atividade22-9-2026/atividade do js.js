d0e3187bf9d7905595961b0bbc3d018f39cf9004
joao-pedro1234567890 <joaopedro1marciano@gmail.com>
2026-09-22 16:42:27 -0300

    Add files via upload

--- atividade22-9-2026/atividade do js.js
+++ atividade22-9-2026/atividade do js.js
@@ -11,5 +11,19 @@ console.log(ehPar);
 if(ehPar===0){alert("e par")}
 else{alert("e impar")}
 }
-
+function atv3(){
+let C=Number(prompt("dijite pra ser convertido pra farenhaite"));
+let F=(C*1.8)+32;
+console.log(F+" "+C);
+alert("deu "+F);
+}
+function atv4(){
+let peso=Number(prompt("dijete o peso"));
+let altura=Number(prompt("dijite a altura"));
+let imc = peso/(altura*altura);
+console.log(imc);
+if(imc<18.5){alert("abaixo do peso");};
+if(imc>25){alert("acima do peso");};
+if(imc<25 &&imc>18.5){alert("peso ideal");};
+}
 
