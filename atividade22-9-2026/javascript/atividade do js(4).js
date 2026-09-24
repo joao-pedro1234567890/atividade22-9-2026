e4188aa601f9c434ada3161974a5e2b7d668a7b1
joao-pedro1234567890 <joaopedro1marciano@gmail.com>
2026-09-24 15:38:01 -0300

    Refactor atv4 function for improved readability

--- atividade22-9-2026/javascript/atividade do js(4).js
+++ atividade22-9-2026/javascript/atividade do js(4).js
@@ -4,14 +4,14 @@ function atv4(){
     let altura=Number(prompt("dijite a altura"));
     let imc = peso/(altura*altura);
     console.log(imc);
-if(imc<18.5){
-    alert("abaixo do peso");
-};
-if(imc>25){
-    alert("acima do peso");
-};
-if(imc<25 &&imc>18.5){
-    alert("peso ideal");
-};
+    if(imc<18.5){
+        alert("abaixo do peso");
+    };
+    if(imc>25){
+        alert("acima do peso");
+    };
+    if(imc<25 &&imc>18.5){
+        alert("peso ideal");
+    };
 }
 
